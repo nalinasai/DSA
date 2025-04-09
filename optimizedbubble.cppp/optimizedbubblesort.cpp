99c1e0979f7e65bf146a2523b047b309696fce20
nalinasai <nalinanalina995@gmail.com>
2025-04-09 08:18:20 +0530

    optimized bubble sort code

---
+++ optimizedbubble.cppp/optimizedbubblesort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>  // Includes the input-output stream library for printing to the console
+#include <vector>
+#include <sstream>
+using namespace std; // To use standard C++ library functions like cout, endl without needing std:: prefix
+
+int main(){
+
+    cout<< "Enter the input: ";
+    vector<int> numbers;
+    string inputs;
+
+    getline(cin , inputs);
+
+    stringstream stream(inputs);
+
+    int num;
+    while(stream>>num){
+        numbers.push_back(num);
+    }
+    
+    int flag;
+    int n = numbers.size();
+    for (int i=0; i<n-1; i++){
+        flag = 0 ;
+        for (int j=0; j<n-i-1; j++){
+            if(numbers[j]>numbers[j+1]){
+                swap(numbers[j],numbers[j+1]);
+                flag = 1 ;
+            }
+        }
+        if(flag==0){
+            break;
+        }
+    }
+
+    cout << "after sorted: ";
+    for(int k=0; k<n; k++){
+        cout << numbers[k]<< " ";
+    }
+}
+
